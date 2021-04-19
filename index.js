a16afc913031275408a0d152792fda1a6d75cff0
Francis Chong <francis@goodnotesapp.com>
2021-04-20 00:09:30 +0100

    disable wasm

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const js = require('./sha512.js')
-const wasm = require('sha512-wasm')
 
 var Proto = js
 
@@ -8,19 +7,7 @@ module.exports = function () {
 }
 
 module.exports.ready = function (cb) {
-  wasm.ready(function () { // ignore errors
-    cb()
-  })
 }
 
-module.exports.WASM_SUPPORTED = wasm.SUPPORTED
+module.exports.WASM_SUPPORTED = false
 module.exports.WASM_LOADED = false
-
-var SHA512_BYTES = module.exports.SHA512_BYTES = 64
-
-wasm.ready(function (err) {
-  if (!err) {
-    module.exports.WASM_LOADED = true
-    module.exports = Proto = wasm
-  }
-})
